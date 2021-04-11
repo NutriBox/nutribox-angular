2cc79f8c433a02d77e464ae9c692deee36c05e6e
Edilberto Lima <edilberto.lima@gmail.com>
2021-04-11 20:14:56 -0300

    config environment service heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   HTTP_API_MOCK: 'http://localhost:3000',
-  HTTP_API: 'http://localhost:8080/api/',
-  HTTP_FUN: 'http://localhost:8080/'
+  HTTP_API: 'https://nutribox-nbx.herokuapp.com/api/',
+  HTTP_FUN: 'https://nutribox-nbx.herokuapp.com/'
 };
